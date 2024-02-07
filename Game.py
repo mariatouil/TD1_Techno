860e91cad9bb9973dda1d1aadf86d50ba8d7d9e1
maria touilzak <mariatouilzak19@gmail.com>
2024-02-07 15:30:51 +0100

    creation de la classe game

---
+++ Game.py
@@ -0,0 +1,13 @@
+class Game:
+    history_file = "game_history.txt"
+
+    def __init__(self):
+        self.__game_status = "Not started"
+
+    def config(self):
+        # Configure la partie et sauvegarde dans le fichier texte
+        print("Configuring the game...")
+        # Votre logique de configuration ici
+        self.__game_status = "Configured"
+        print("Game configured.")
+
