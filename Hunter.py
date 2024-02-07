854f6a8743453e3b9282af2a83655c63068e6e91
ranya <20235451@etud.univ-evry.fr>
2024-02-07 13:47:26 +0100

    creation de la class hunter

---
+++ Hunter.py
@@ -0,0 +1,17 @@
+import random
+
+class Hunter:
+    def __init__(self):
+        self.degat = random.randint(1, 2)
+        self.chance = 10
+        self.fuite = 20
+        self.prix = 25
+        self.type_unite = "hunter"
+
+    def __str__(self):
+        return f"Dégât: {self.degat}, Chance: {self.chance}, Fuite: {self.fuite}, Prix: {self.prix}, Type d'unité: {self.type_unite}"
+
+# Exemple d'utilisation
+if __name__ == "__main__":
+    hunter = Hunter()
+    print(hunter)
