83000456ee10656378a72e7b7864ddeb4f45ca19
ranya <20235451@etud.univ-evry.fr>
2024-02-07 14:40:27 +0100

    creation de la classe team

---
+++ Team.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class Team(ABC):
+    def init(self, members):
+        self._members = members
+
+    def len(self):
+        return len(self._members)
+
+    def getitem(self, index):
+        return self._members[index]
